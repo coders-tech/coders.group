fbdd3c3497fd76bc400694c4bc721f63c870facc
Nikia Atroshenko <atroshenkonikita@gmail.com>
2019-07-17 19:12:36 +0300

    Responsive and Butiful design added and tested on devices with different screens

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,14 +1,19 @@
 import React from "react";
-import '../skills.css'
+import '../skills.css';
+import backend from  '../img/backend2.png';
+import frontend from  '../img/frontend.png';
+import uiux from  '../img/uiux.png';
+import mobdev from  '../img/mobdev.png';
 
 const Skills = () => {
   return (
     <div id="section-2">
       <div className="container1">
+      <h1>Skills</h1>
         <div className="frontend">
-          <img src="frontend.png" className="skillimg leftimg" />
+          <img src={frontend} alt="Frontend" className="skillimg leftimg" />
           <div className="text skillright">
-            <h1>Frontend</h1>
+            <h2>Frontend</h2>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum eius
               corporis laborum assumenda hic veritatis aut perferendis,
@@ -19,7 +24,7 @@ const Skills = () => {
         </div>
         <div className="backend">
           <div className="text skillleft">
-            <h1>Backend</h1>
+            <h2>Backend</h2>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum eius
               corporis laborum assumenda hic veritatis aut perferendis,
@@ -27,12 +32,12 @@ const Skills = () => {
               optio ratione? Quas, dolorem libero!
             </p>
           </div>
-          <img src="backend2.png" className="skillimg rightimg" />
+          <img src={backend} alt="Backend" className="skillimg rightimg" />
         </div>
         <div className="uiux">
-          <img src="uiux.png" className="skillimg leftimg" />
+        <img src={uiux} alt="UI/UX" className="skillimg leftimg" />
           <div className="text skillright">
-            <h1>UX/UI</h1>
+            <h2>UX/UI</h2>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum eius
               corporis laborum assumenda hic veritatis aut perferendis,
@@ -43,7 +48,7 @@ const Skills = () => {
         </div>
         <div className="mobapp">
           <div className="text skillleft">
-            <h1>Mobile Development</h1>
+            <h2>Mobile Development</h2>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum eius
               corporis laborum assumenda hic veritatis aut perferendis,
@@ -51,7 +56,7 @@ const Skills = () => {
               optio ratione? Quas, dolorem libero!
             </p>
           </div>
-          <img src="mobdev.png" className="rightimg skillimg" />
+          <img src={mobdev} alt="Mobile Development" className="rightimg skillimg" />
         </div>
       </div>
     </div>
