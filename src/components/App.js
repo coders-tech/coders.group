4d5065efcbffa8d22d85ca6044952e7748e4ea33
maximov8 <denismaximov98@gmail.com>
2019-07-25 09:55:54 +0300

    rendering different layout based on what device is being used(for responsive mobile application)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Sections from './Sections'
 import Nav from './Nav'
 
+let size = window.innerWidth < 1024
+
 const App = () => {
+    const [isMobile, setMobile] = useState(size)
+
+    useEffect(() => {
+        let handleResize = () => {
+            setMobile(window.innerWidth < 1024)
+        }
+        console.log('rendered')
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [setMobile])
     return(
-        <div className='container'>
+        isMobile ? (<h1>Hello</h1>) :
+        (<div className='container'>
             <Nav />
             <Sections />
-        </div>
+        </div>)
     )
 }
 
