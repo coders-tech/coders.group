64cabe37750683807b3f5735208a3b1e03ca9a04
maximov8 <denismaximov98@gmail.com>
2019-07-25 14:20:29 +0300

    we will do it with media queries

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import Sections from './Sections'
 import Nav from './Nav'
 
-let size = window.innerWidth < 1024
-
 const App = () => {
-    const [isMobile, setMobile] = useState(size)
-
-    useEffect(() => {
-        let handleResize = () => {
-            setMobile(window.innerWidth < 1024)
-        }
-        console.log('rendered')
-        window.addEventListener("resize", handleResize)
-
-        return () => {
-            window.removeEventListener("resize", handleResize)
-        }
-    }, [setMobile])
+  
     return(
-        isMobile ? (<h1>Hello</h1>) :
-        (<div className='container'>
+        <div className='container'>
             <Nav />
             <Sections />
-        </div>)
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Menu from './Menu'
-import {motion} from 'framer-motion'
+import Menu from "./Menu";
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
     <div id="section-1">
-      
       <div className="left">
-
-          <motion.h1
-            style={{scale: 0}}
-            animate={{scale:1}}
-            transition={{duration: 0.5}}
-          >CODERS/<span id="logo">GROUP</span></motion.h1>
-
+        <motion.h1
+          style={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          CODERS/<span id="logo">GROUP</span>
+        </motion.h1>
 
         <p className="animated fadeIn delay-1s coders">
-        At coders.group we combine technology, creativity and strategy to transform your ideas into products.
+          At coders.group we combine technology, creativity and strategy to
+          transform your ideas into products.
         </p>
       </div>
       <div className="right" />
