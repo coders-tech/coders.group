770cd7409aae124946e37b16219aada118b52fcc
Nikia Atroshenko <atroshenkonikita@gmail.com>
2019-07-29 11:51:23 +0300

    mobile is working for 2 components

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,21 @@ const Header = () => {
           transform your ideas into products.
         </p>
       </div>
-      <div className="right" />
+      <div className="right"> 
+      <div className="TextOnTablets">
+         <motion.h1
+          style={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          CODERS/<span id="logo">GROUP</span>
+        </motion.h1>
+
+        <p className="animated fadeIn delay-1s coders">
+          At coders.group we combine technology, creativity and strategy to
+          transform your ideas into products.
+        </p>
+      </div></div>
       <Menu />
     </div>
   );
