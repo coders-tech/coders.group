e80ea930e66a05fea2ceb611496e0f3541aeffe2
maximov8 <denismaximov98@gmail.com>
2019-08-09 14:36:23 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,25 +4,12 @@ import '../footer.css'
 const Footer = () => {
     return(
         <div id="section-4">
-
             <div className='footer'>
-            <div>
-                    <p>© coders.group</p>
-                    
-                </div>
-                <div>
-                    <p>follow us on:</p>
-                    <ul className='footerNav'>
-                        <li><a href='#'>a</a></li>
-                        <li><a href='#'>a</a></li>
-                        <li><a href='#'>a</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <p>contact us</p>
-                        <p>phone: +4473618128372</p>
-                        <p> email: office@coders.group</p>
-                </div>
+                <ul>
+                    <li>team@coders.group</li>
+                    <li>+3598934123123124</li>
+                    <li>+3598934123123124</li>
+                </ul>
             </div>
         </div>
     )
